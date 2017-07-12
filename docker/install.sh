1a618b65e90191e912a73355c23faa2ffe72cdd6
Jon Jagger <jon@jaggersoft.com>
2017-07-12 08:36:21 +0100

    added --no-install-recommends to apt-get commands in Dockerfile for smaller image

--- docker/install.sh
+++ docker/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y wget clang-3.6 libicu-dev libxml2
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
+readonly CLANG_VERSION=3.6
 
-SWIFT_SNAPSHOT_URL_DIRECTORY=swift-3.1.1-RELEASE
-SWIFT_SNAPSHOT=${SWIFT_SNAPSHOT_URL_DIRECTORY}-ubuntu14.04
+apt-get update --no-install-recommends
+apt-get install --no-install-recommends --yes wget clang-${CLANG_VERSION} libicu-dev libxml2
+update-alternatives --install /usr/bin/clang   clang   /usr/bin/clang-${CLANG_VERSION}   100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
 
-wget https://swift.org/builds/swift-3.1.1-release/ubuntu1404/${SWIFT_SNAPSHOT_URL_DIRECTORY}/${SWIFT_SNAPSHOT}.tar.gz
+readonly SWIFT_VERSION=3.1.1
+readonly SWIFT_SNAPSHOT_URL_DIRECTORY=swift-${SWIFT_VERSION}-RELEASE
+readonly SWIFT_SNAPSHOT=${SWIFT_SNAPSHOT_URL_DIRECTORY}-ubuntu14.04
+
+wget https://swift.org/builds/swift-${SWIFT_VERSION}-release/ubuntu1404/${SWIFT_SNAPSHOT_URL_DIRECTORY}/${SWIFT_SNAPSHOT}.tar.gz
 
 tar -xvf ${SWIFT_SNAPSHOT}.tar.gz
 chmod -R a+r ${SWIFT_SNAPSHOT}/usr
