72ed054432e304f9a8cb60de9030d51ce30e2187
Jon Jagger <jon@jaggersoft.com>
2020-01-27 12:09:08 +0000

    Upgrade to Ubuntu18.04 and v5.1.3

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 ## image_name="cyberdojofoundation/swift"
 
-[Version=4.0.3](https://github.com/cyber-dojo-languages/swift/blob/master/check_version.sh)
+[Version=5.1.3](https://github.com/cyber-dojo-languages/swift/blob/master/check_version.sh)
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- docker/install.sh
+++ docker/install.sh
@@ -1,21 +1,16 @@
 #!/bin/bash -Eeu
 
-readonly CLANG_VERSION=3.6
+# https://swift.org/download/#releases
+apt-get install --yes clang libicu-dev
 
-apt-get update
-apt-get install --yes wget clang-${CLANG_VERSION} libicu-dev libxml2
-update-alternatives --install /usr/bin/clang   clang   /usr/bin/clang-${CLANG_VERSION}   100
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
+SWIFT_SNAPSHOT_URL_DIRECTORY=swift-${SWIFT_VERSION}-RELEASE
+SWIFT_SNAPSHOT=${SWIFT_SNAPSHOT_URL_DIRECTORY}-ubuntu${SWIFT_OS_MAJOR}.${SWIFT_OS_MINOR}
+SWIFT_URL=https://swift.org/builds/swift-${SWIFT_VERSION}-release/ubuntu${SWIFT_OS_MAJOR}${SWIFT_OS_MINOR}/${SWIFT_SNAPSHOT_URL_DIRECTORY}/${SWIFT_SNAPSHOT}.tar.gz
 
-readonly SWIFT_VERSION=4.0.3
-readonly SWIFT_SNAPSHOT_URL_DIRECTORY=swift-${SWIFT_VERSION}-RELEASE
-readonly SWIFT_SNAPSHOT=${SWIFT_SNAPSHOT_URL_DIRECTORY}-ubuntu16.04
+apt-get install --yes wget
 
-wget https://swift.org/builds/swift-${SWIFT_VERSION}-release/ubuntu1604/${SWIFT_SNAPSHOT_URL_DIRECTORY}/${SWIFT_SNAPSHOT}.tar.gz
+wget ${SWIFT_URL}
+tar xzf ${SWIFT_SNAPSHOT}.tar.gz
+rm ${SWIFT_SNAPSHOT}.tar.gz
 
-tar -xvf ${SWIFT_SNAPSHOT}.tar.gz
-chmod -R a+r ${SWIFT_SNAPSHOT}/usr
-cp -r ${SWIFT_SNAPSHOT}/usr/* /usr/
-
-rm -rf ${SWIFT_SNAPSHOT}
-rm -f ${SWIFT_SNAPSHOT}.tar.gz
+apt-get remove --yes wget
